232e946c3a0136c785bd0b0e79b642583d0099f1
Félix Dupré-Ouellet <f.dupre.ouellet@hotmail.ca>
2023-11-01 15:32:43 -0400

    Add v-app tag to homepage to make it rendre 'correctly'

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,6 +1,8 @@
 <template>
-    <v-app-bar></v-app-bar>
-    <div>
-        Hello
-    </div>
+    <v-app>
+        <v-app-bar></v-app-bar>
+        <div>
+            Hello
+        </div>
+    </v-app>
 </template>
