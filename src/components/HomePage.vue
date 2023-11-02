4160316797cd33ec2a8be244e07d7639fba9b343
Félix Dupré-Ouellet <f.dupre.ouellet@hotmail.ca>
2023-11-02 18:43:23 -0400

    It marchez

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,15 +1,20 @@
 <script setup>
 import Part from '@/util/questionnaire/Part';
 import Answer from '@/util/questionnaire/Answer';
-import QuestionnairePart from './questionnaire/QuestionnairePart.vue';
+// import QuestionnairePart from './questionnaire/QuestionnairePart.vue';
+import DeviceQuestionnaire from './questionnaire/DeviceQuestionnaire.vue';
 
-const question = new Part("Ceci est une question", [new Answer("réponse 1"), new Answer("réponse 2")])
+const autreQuestion1 = new Part("Ceci est la question suivant la réponse 1")
+const autreQuestion2 = new Part("Ceci est la question suivant la réponse 2", [new Answer("raiponce 1", new Part("paf")), new Answer("raiponce 2", new Part("pouf"))])
+
+const question = new Part("Ceci est une question", [new Answer("réponse 1", autreQuestion1), new Answer("réponse 2", autreQuestion2)])
 </script>
 
 <template>
   <v-app>
     <v-app-bar></v-app-bar>
     <div>Hello</div>
-    <QuestionnairePart v-bind:part=question></QuestionnairePart>
+    <DeviceQuestionnaire v-bind:firstPart=question></DeviceQuestionnaire>
+    <!-- <QuestionnairePart v-bind:part=question v-bind:reactToAnswer=reactToAnswer></QuestionnairePart> -->
   </v-app>
 </template>
