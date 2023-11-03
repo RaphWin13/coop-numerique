8de87ac8746fcc4bca6d7c5f914aba2829b61c64
Félix Dupré-Ouellet <f.dupre.ouellet@hotmail.ca>
2023-11-03 13:05:39 -0400

    Add questionnaire component (#1)
    * Add questionnaire objects + Prettier
    
    * Populate data for smartphone questionnaire + fix formatting options
    
    * Very WIP, but working radio buttons from data
    
    * It marchez
    
    * Add id to each part to fix bug in v-for
    
    * Add missing question text and fix bug with html ids

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,8 +1,15 @@
+<script setup>
+import { smartphoneRootQuestion } from "../data/questions/smartphone";
+import DeviceQuestionnaire from "./questionnaire/DeviceQuestionnaire.vue";
+
+const rootQuestion = smartphoneRootQuestion;
+</script>
+
 <template>
-    <v-app>
-        <v-app-bar></v-app-bar>
-        <div>
-            Hello
-        </div>
-    </v-app>
+  <v-app>
+    <v-app-bar title="Co-op numérique"></v-app-bar>
+    <v-main class="d-flex flex-column align-center">
+      <DeviceQuestionnaire v-bind:firstPart="rootQuestion"></DeviceQuestionnaire>
+    </v-main>
+  </v-app>
 </template>
---
+++ src/components/questionnaire/QuestionnairePart.vue
@@ -0,0 +1,18 @@
+<template>
+  <div style="padding: 1em;">
+    <b style="margin: 1em">{{ part.text }}</b>
+  </div>
+  <div>
+    <v-radio-group style="display: inline-block" :inline=true>
+      <div v-for="(answer, index) in part.answers" :key="index">
+        <v-radio @input="reactToAnswer($event, partPosition, index)" :label="answer.text" :value="index"></v-radio>
+      </div>
+    </v-radio-group>
+  </div>
+</template>
+
+<script setup>
+defineProps(["part", "partPosition", "reactToAnswer"]);
+</script>
+
+<style></style>
