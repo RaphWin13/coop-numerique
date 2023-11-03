cf37c51dba60b4037172dbfd7ad76619c6d822a8
Raphaël Guay-Gagné <raphael.guay-gagne.1@ulaval.ca>
2023-11-03 12:23:07 -0400

    add serviceType component for HomePage

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,8 +1,37 @@
 <template>
-    <v-card class="ma-5 mt-10" elevation="0">
+    <v-card class="mx-15 mt-10 pb-5" elevation="0">
         <v-card-title class="text-center">
             <p class="text-h3 font-weight-black">Coop-numérique</p>
         </v-card-title>
-        <v-card-subtitle class="text-center">Coop de l'Université Laval</v-card-subtitle>
+        <v-card-subtitle class="mb-10 text-center">Coop de l'Université Laval</v-card-subtitle>
+        <v-row class="d-flex justify-center">
+            <ServiceCard 
+                v-for="service in services"
+                :key=service
+                :typeService=service.type
+                :iconTitle=service.icon
+                :question=service.question
+            />
+        </v-row>
     </v-card>
 </template>
+
+<script>
+import ServiceCard from './ServiceCard.vue';
+
+    export default {
+        name: "HomePage",
+        components: {
+            ServiceCard
+        },
+        data() {
+            return {
+                services: [
+                    {"type": "Diagnostique", "question": "Vous souhaitez diagnostiquer un appareil?", "icon": "mdi-magnify"},
+                    {"type": "Prendre un RDV", "question": "vous souhaitez prendre rendez-vous avec l'un de nos spécialistes?", "icon": "mdi-calendar"},
+                    {"type": "Plus d'informations", "question": "En apprendre plus sur les problèmes récurrents des appareils électroniques modernes et comment les réparer soi-même", "icon": "mdi-laptop"}
+                ]
+            }
+        }
+    }
+</script>
---
+++ src/components/ServiceCard.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-hover
+        v-slot="{ isHovering, props }"
+    >
+        <v-card 
+            variant="outlined"
+            :class="{ 'on-hover': isHovering }"
+            class="mb-5 mx-1"
+            style="cursor: pointer;"
+            width="50%"
+            v-bind="props"
+        >
+            <v-card-title>
+                <v-icon :icon=iconTitle class="mr-2"></v-icon>
+                {{ typeService }}
+            </v-card-title>
+            <v-card-text>{{ question }}</v-card-text>
+        </v-card>
+    </v-hover>
+</template>
+
+<script>
+
+    export default {
+        name: "ServiceCard",
+        props: {
+            typeService: String,
+            iconTitle: String,
+            question: String
+        }
+    }
+</script>
+
+<style scoped>
+
+.on-hover {
+    transition: 0.2s ease-in-out;
+    transform: scale(1.1);
+}
+</style>
