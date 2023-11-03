1b1e20e4394e078ec6be98f74977f58744e99013
Raphaël Guay-Gagné <raphael.guay-gagne.1@ulaval.ca>
2023-11-03 11:23:29 -0400

    add homePage component

--- src/components/HomeNavbar.vue
+++ src/components/HomeNavbar.vue
@@ -2,7 +2,7 @@
     <v-toolbar
         color="white"
     >
-        <v-toolbar-title>{{ appName }}</v-toolbar-title>
+        <img height="50" class="ma-5" :src="require('../assets/light-bulb.png')"/>
         <v-spacer></v-spacer>
         <v-toolbar-items>
             <v-btn
@@ -13,6 +13,11 @@
             >
             <v-icon left :icon="item.icon" size="large" style="padding-right: 10%;"></v-icon>
             {{ item.name }}
+            </v-btn>
+            <v-btn
+                style="cursor: pointer;"
+            >
+                <v-icon icon="mdi-account-circle" size="x-large"></v-icon>
             </v-btn>
         </v-toolbar-items>
     </v-toolbar>
@@ -26,7 +31,8 @@
                 appName: "Coop numérique",
                 navbarItems: [
                     { name: "home", route: "/home", icon: "mdi-home"},
-                    { name: "diagnostique", route: "/diagnostique", icon: "mdi-card-search-outline"},
+                    { name: "recycler", route: "/recycler", icon: "mdi-recycle"},
+                    { name: "diagnostique", route: "/diagnostique", icon: "mdi-card-search"},
                     { name: "rdv", route: "/rdv", icon: "mdi-calendar" },
                     { name: "à propos", route: "/aPropos", icon: "mdi-information" }
                 ]
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,5 +1,8 @@
 <template>
-    <v-card>
-        
+    <v-card class="ma-5 mt-10" elevation="0">
+        <v-card-title class="text-center">
+            <p class="text-h3 font-weight-black">Coop-numérique</p>
+        </v-card-title>
+        <v-card-subtitle class="text-center">Coop de l'Université Laval</v-card-subtitle>
     </v-card>
 </template>
