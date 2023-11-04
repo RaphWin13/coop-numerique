dcdfd0ac2f54bda900adb297ca5f253563596493
Laurence <laurence.delisle@menya.levio.ca>
2023-11-04 12:49:43 -0400

    Added decision trees for computer and console. Translated smartphone.

--- src/data/questions/smartphone.js
+++ src/data/questions/smartphone.js
@@ -2,89 +2,91 @@ import Part from "@/util/questionnaire/Part";
 import Answer from "@/util/questionnaire/Answer";
 
 // reuseable text
-const allGoodText = "All good!";
-const doesNotWorkText = "Does not work";
-const deviceAgeText = "What is the age of the device?";
+const allGoodText = "Tout est beau!";
+const doesNotWorkText = "Ne fonctionne pas";
+const deviceAgeText = "Quel est l'âge de l'appareil?";
 
 // resolutions
-const replaceResolution = new Part("Replace.");
-const repairResolution = new Part("Repair.");
-const dontDoAnythingOrReplaceScreenResolution = new Part("Don't do anything for now or replace the screen.");
-const replaceOrRepairResolution = new Part("Replace or repair.");
-const notSureResolution = new Part("Not sure here.");
-const replaceBatteryResolution = new Part("Replace battery.");
-const waitABitResolution = new Part("Wait a bit.");
-const saveBatteryResolution = new Part("Battery saving mode, reduce brightness, disable apps that use a lot of battery.");
+const replaceResolution = new Part("Il faudrait remplacer l'appareil.");
+const repairResolution = new Part("Il faudrait réparer l'appareil.");
+const dontDoAnythingOrReplaceScreenResolution = new Part("Ne rien faire ou faire réparer l'écran.");
+const replaceOrRepairResolution = new Part("Il faudrait réparer ou remplacer selon l'âge.");
+const replaceBatteryResolution = new Part("Il faudrait remplacer la batterie.");
+const waitABitResolution = new Part("Attendre un peu.");
+//const saveBatteryResolution = new Part("Essayer le mode d'économie de batterie, réduire la clarté de l'écran, désactiver les applications qui demandent beaucoup d'énergie.");
 
 // questions and answers
 const allGoodAnswer = new Answer(allGoodText, new Part("Excellent!"));
 
 // Overheating and usage
 const checkForUpdatesDoesNotWorkQuestion = new Part(deviceAgeText, [
-  new Answer("Older than 2 years", replaceResolution),
-  new Answer("Younger than 2 years", repairResolution),
+  new Answer("Plus vieux que 2 ans.", replaceResolution),
+  new Answer("Plus jeune que 2 ans.", repairResolution),
 ]);
 const checkForUpdatesDoesNotWorkAnswer = new Answer(doesNotWorkText, checkForUpdatesDoesNotWorkQuestion);
 
 const checkForUpdatesQuestion = new Part(
-  "Check for updates, delete and download again, check storage space, clear things running in the background, update phone OS, turn phone off then on again. Shut down apps (lower RAM usage), delete unnecessary files.",
+  "Vérifier que toutes les mises à jour ont été effectuées, supprimer et retélécharger, vérifier l'espace de stockage, désactiver les applications qui roulent en arrière plan, essayer d'éteindre puis de redémarrer le téléphone. Éteindre les applications, supprimer le contenu inutile.",
   [allGoodAnswer, checkForUpdatesDoesNotWorkAnswer],
 );
 
-const buttonResetCombinationQuestion = new Part("Try some button reset combination", [allGoodAnswer, new Answer(doesNotWorkText, replaceResolution)]);
-const chargeAndRetryQuestion = new Part("Charge it and retry.", [allGoodAnswer, new Answer(doesNotWorkText, buttonResetCombinationQuestion)]);
+const buttonResetCombinationQuestion = new Part("Essayer de redémarrer l'appareil.", [allGoodAnswer, new Answer(doesNotWorkText, replaceResolution)]);
+const chargeAndRetryQuestion = new Part("Charger l'appareil et réessayer.", [allGoodAnswer, new Answer(doesNotWorkText, buttonResetCombinationQuestion)]);
 
-const usageQuestion = new Part("", [
-  new Answer("Slow", checkForUpdatesQuestion),
-  new Answer("App crashes", checkForUpdatesQuestion),
-  new Answer("Cannot turn on", chargeAndRetryQuestion),
+const usageQuestion = new Part("Symptômes", [
+  new Answer("Lenteur", checkForUpdatesQuestion),
+  new Answer("Application ne fonctionne pas", checkForUpdatesQuestion),
+  new Answer("Difficulté à s'allumer", chargeAndRetryQuestion),
 ]);
 
 // Physical problem
-const blackSpotsAnswer = new Answer("Black spots, blurring, discoloration > comfort", dontDoAnythingOrReplaceScreenResolution);
-const blackSpotsQuestion = new Part("", [blackSpotsAnswer]);
+const blackSpotsAnswer = new Answer("Points noirs, flou, décoloration.", dontDoAnythingOrReplaceScreenResolution);
+const blackSpotsQuestion = new Part("Symptômes", [blackSpotsAnswer]);
 
-const chargingPortQuestion = new Part("Clean it with new toothbrush or dry cotton swab", [allGoodAnswer, new Answer(doesNotWorkText, replaceOrRepairResolution)]);
+const chargingPortQuestion = new Part("Nettoyer avec une brosse à dent neuve ou un cotton tige.", [allGoodAnswer, new Answer(doesNotWorkText, replaceOrRepairResolution)]);
 
-const physicalProblemQuestion = new Part("", [
-  new Answer("Buttons", repairResolution),
-  new Answer("Screen", blackSpotsQuestion),
-  new Answer("Charging port", chargingPortQuestion),
-  new Answer("Broken mic", notSureResolution),
+const physicalProblemQuestion = new Part("Composante problématique", [
+  new Answer("Boutons", repairResolution),
+  new Answer("Écran", blackSpotsQuestion),
+  new Answer("Port de charge", chargingPortQuestion)
 ]);
 
 // Battery drainage
-const eightyPercentBatteryQuestion = new Part("Loses more than 80% of its battery in one day", [
-  new Answer("Yes", replaceBatteryResolution),
-  new Answer("No", waitABitResolution),
+const eightyPercentBatteryQuestion = new Part("Est-ce que l'appareil perd plus de 80% de sa batterie en une journée?", [
+  new Answer("Oui", replaceBatteryResolution),
+  new Answer("Non", waitABitResolution),
 ]);
 
+const saveBattery = new Part("Essayer le mode d'économie de batterie, réduire la clarté de l'écran, désactiver les applications qui demandent beaucoup d'énergie.",
+[allGoodAnswer, new Answer("Ne fonctionne pas.", new Part("Il faudrait changer la batterie."))])
+
 const batteryDrainageQuestion = new Part(deviceAgeText, [
-  new Answer("Older than 2 years", eightyPercentBatteryQuestion),
-  new Answer("More recent than 2 years", saveBatteryResolution),
+  new Answer("Plus vieux que 2 ans", eightyPercentBatteryQuestion),
+  new Answer("Plus jeune que 2 years", saveBattery),
 ]);
 
 // Connectivity
-const tryRestartingQuestion = new Part("Try restarting.", [
+const tryRestartingQuestion = new Part("Essayer de le faire redémarrer.", [
   allGoodAnswer,
-  new Answer(doesNotWorkText, new Part("Try enabling airplane mode for at least 1 minute, restart wifi and connect (can restart router also).")),
+  new Answer(doesNotWorkText, new Part("Essayer d'activer le mode avion pour au moins une minute, redémarrer le wifi and reconnecter.", 
+  [allGoodAnswer, new Answer("Ne fonctionne pas.", repairResolution)])),
 ]);
 
 // Water
 const turnPhoneOffQuestion = new Part(
-  "Turn phone off, shake, dry it in front of fan, if still weird then replace phone. Do not turn on, remove battery if possible.",
+  "Éteindre le téléphone, sécher le téléphone à l'aide d'un ventilateur. Ne pas allumer le téléphone, retirer la batterie si possible.",
   [allGoodAnswer, new Answer(doesNotWorkText, replaceResolution)],
 );
 
 // Root
-const overheatingAnswer = new Answer("Overheating", checkForUpdatesQuestion);
-const usageAnswer = new Answer("Usage", usageQuestion);
-const physicalProblemAnswer = new Answer("Physical problem", physicalProblemQuestion);
-const batteryDrainageAnswer = new Answer("Battery drainage", batteryDrainageQuestion);
-const connectivityAnswer = new Answer("Connectivity", tryRestartingQuestion);
-const waterAnswer = new Answer("Water", turnPhoneOffQuestion);
+const overheatingAnswer = new Answer("Surchauffe", checkForUpdatesQuestion);
+const usageAnswer = new Answer("Utilisation", usageQuestion);
+const physicalProblemAnswer = new Answer("Problème physique", physicalProblemQuestion);
+const batteryDrainageAnswer = new Answer("Drainage de la batterie", batteryDrainageQuestion);
+const connectivityAnswer = new Answer("Connectivité", tryRestartingQuestion);
+const waterAnswer = new Answer("Eau", turnPhoneOffQuestion);
 
-export const smartphoneRootQuestion = new Part("Type of problem", [
+export const smartphoneRootQuestion = new Part("Type de problème", [
   overheatingAnswer,
   usageAnswer,
   physicalProblemAnswer,
